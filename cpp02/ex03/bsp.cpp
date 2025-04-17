dc5065675833dd53bece35d95e931ed7a4f4e5a8
Thant Htet Aung <maung.good@gmail.com>
2025-04-17 13:10:09 +0800

    on the edge added but == yet to add

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -3,34 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   bsp.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taung <taung@student.42singapore.sg>       +#+  +:+       +#+        */
+/*   By: taung <taung@student.42singapore.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 20:00:15 by taung             #+#    #+#             */
-/*   Updated: 2025/04/15 13:48:45 by taung            ###   ########.fr       */
+/*   Updated: 2025/04/17 12:59:47 by taung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
+// Already good
 Fixed abs(Fixed numb) {
 	if (numb.toFloat() < 0)
 		return (numb * -1);
 	return (numb);
 }
 
+// Already good
 Fixed	getArea(Point p1, Point p2, Point p3) {
-	/*
-		Area = 1/2 |x1(y2-y3)+x2(y3-y1)+x3(y1-y2)|
-	*/
 	Fixed area = (abs(p1.getX() * (p2.getY() - p3.getY())
 					+ p2.getX() * (p3.getY() - p1.getY())
 					+ p3.getX() * (p1.getY() - p2.getY()))) / 2;
-	std::cout << "area: " << area << std::endl;
 	return (area);
 }
 
+// New helper: Check if point is on the line segment between p1 and p2
+bool isOnEdge(Point const p1, Point const p2, Point const point) {
+	Fixed area = getArea(p1, p2, point);
+	if (area != Fixed(0))
+		return false; // not colinear
+
+	// additionally check if point is between p1 and p2
+	Fixed minX = (p1.getX() < p2.getX()) ? p1.getX() : p2.getX();
+	Fixed maxX = (p1.getX() > p2.getX()) ? p1.getX() : p2.getX();
+	Fixed minY = (p1.getY() < p2.getY()) ? p1.getY() : p2.getY();
+	Fixed maxY = (p1.getY() > p2.getY()) ? p1.getY() : p2.getY();
+
+	if (point.getX() < minX || point.getX() > maxX || point.getY() < minY || point.getY() > maxY)
+		return false; // outside the segment
+
+	return true; // colinear and between p1 and p2
+}
+
 bool bsp( Point const a, Point const b, Point const c, Point const point) {
-	if (getArea(a,b,c) == (getArea(a,b,point) + getArea(b,c, point) + getArea(a,c,point)))
-		return (true);
-	return (false);
+	// If point is on any edge or matches any vertex, return false
+	if (isOnEdge(a, b, point) || isOnEdge(b, c, point) || isOnEdge(a, c, point))
+		return false;
+
+	if ((point.getX() == a.getX() && point.getY() == a.getY()) ||
+		(point.getX() == b.getX() && point.getY() == b.getY()) ||
+		(point.getX() == c.getX() && point.getY() == c.getY()))
+		return false;
+
+	// Regular inside check
+	if (getArea(a,b,c) == (getArea(a,b,point) + getArea(b,c,point) + getArea(a,c,point)))
+		return true;
+	return false;
 }
--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taung <taung@student.42singapore.sg>       +#+  +:+       +#+        */
+/*   By: taung <taung@student.42singapore.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 16:27:41 by taung             #+#    #+#             */
-/*   Updated: 2025/04/15 13:51:08 by taung            ###   ########.fr       */
+/*   Updated: 2025/04/17 13:04:01 by taung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,11 @@
 bool bsp( Point const a, Point const b, Point const c, Point const point);
 
 int main( void ) {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	Point	c(1,1),d(5,3),e(2,5),point(30,3);
-	bsp(e,d,c,point) ? std::cout << "in" << std::endl : std::cout << "out" << std::endl;
+	Point	a(1,2),b(5,2),c(4,5),point(3,3);
+	bsp(c,b,a,point) ? std::cout << "in" << std::endl : std::cout << "out" << std::endl;
+	point = Point(1,2);
+	bsp(c,b,a,point) ? std::cout << "in" << std::endl : std::cout << "out" << std::endl;
+	point = Point(2,3);
+	bsp(c,b,a,point) ? std::cout << "in" << std::endl : std::cout << "out" << std::endl;
 	return 0;
 }
