2b2afe6c4431e239e18286b863049c7f03a2d650
Thant Htet Aung <taung@student.42singapore.sg>
2025-09-02 15:23:38 +0800

    cpp06: ex00 a few fixes

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: taung <taung@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 12:01:42 by taung             #+#    #+#             */
-/*   Updated: 2025/09/02 12:42:29 by taung            ###   ########.fr       */
+/*   Updated: 2025/09/02 14:33:21 by taung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,15 @@ to use anything you like for the random choice implementation.
 Base*	generate(void) {
 	switch (getRandom123())
 	{
-	case 1:
-		std::cout << "Class A is generated" << std::endl;
-		return (dynamic_cast<Base*>(new A()));
-	case 2:
-		std::cout << "Class B is generated" << std::endl;
-		return (dynamic_cast<Base*>(new B()));
-	case 3:
-		std::cout << "Class C is generated" << std::endl;
-		return (dynamic_cast<Base*>(new C()));
+		case 1:
+			std::cout << "Class A is generated" << std::endl;
+			return (dynamic_cast<Base*>(new A()));
+		case 2:
+			std::cout << "Class B is generated" << std::endl;
+			return (dynamic_cast<Base*>(new B()));
+		case 3:
+			std::cout << "Class C is generated" << std::endl;
+			return (dynamic_cast<Base*>(new C()));
 	}
 	return NULL;
 }
@@ -56,9 +56,9 @@ It prints the actual type of the object pointed to by p: "A", "B", or "C".
 void	identify(Base* p) {
 	if (dynamic_cast<A*>(p))
 		std::cout << "A" << std::endl;
-	if (dynamic_cast<B*>(p))
+	else if (dynamic_cast<B*>(p))
 		std::cout << "B" << std::endl;
-	if (dynamic_cast<C*>(p))
+	else if (dynamic_cast<C*>(p))
 		std::cout << "C" << std::endl;
 }
 
@@ -68,42 +68,24 @@ It prints the actual type of the object referenced by p: "A", "B", or "C". Using
 inside this function is forbidden.
 */
 void	identify(Base& p) {
-	int	identifier = 3;
 
 	try {
-		dynamic_cast<A&>(p);
-	} catch (const std::exception& e) {
-		identifier--;
-	}
-
-	if (identifier == 3) {
+		const A tmp = dynamic_cast<A&>(p);
 		std::cout << "A" << std::endl;
 		return;
-	}
+	} catch (const std::exception& e) {}
 
 	try {
-		dynamic_cast<B&>(p);
-	} catch (const std::exception& e) {
-		identifier--;
-	}
-
-	if (identifier == 2) {
+		const B tmp = dynamic_cast<B&>(p);
 		std::cout << "B" << std::endl;
 		return;
-	}
+	} catch (const std::exception& e) {}
 
 	try {
-		dynamic_cast<C&>(p);
-	} catch (const std::exception& e) {
-		identifier--;
-	}
-
-	if (identifier == 1) {
+		const C tmp = dynamic_cast<C&>(p);
 		std::cout << "C" << std::endl;
 		return;
-	}
-	else
-		std::cerr << "err: not a valid derived class" << std::endl;
+	} catch (const std::exception& e) {}
 }
 
 int main() {
